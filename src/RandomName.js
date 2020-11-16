7c0ce39592862e567b543867f6ef7ef8dccb3506
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-11-16 10:35:38 -0800

    data

---
+++ src/RandomName.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RandomName extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
