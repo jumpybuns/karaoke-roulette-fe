83a7c80baa00d80b6d4f2ac289a1cdd1178e48d7
Jumpybuns <ethanpiercepresents@gmail.com>
2020-11-18 16:58:56 -0800

    adventures of links

--- src/Header.js
+++ src/Header.js
@@ -10,19 +10,19 @@ export default class Header extends Component {
             <div className='header'>
                 <ul className='headerul'>
 
-                    <li><NavLink  to='/home'  activeStyle={{display: 'none'}}>Home</NavLink> </li>
-                    
-                    <li><NavLink to='/videos'  activeStyle={{display: 'none'}}>Karaoke Room</NavLink> </li>
+                    <li><NavLink to='/home' activeStyle={{ display: 'none' }}>Home</NavLink> </li>
 
-                    <li><NavLink  to='/favorites'  activeStyle={{display: 'none'}}>Favorites</NavLink> </li>
+                    <li><NavLink to='/videos' activeStyle={{ display: 'none' }}>Karaoke Room</NavLink> </li>
 
-                    <li><NavLink  to='/aboutus'  activeStyle={{display: 'none'}}> About Us </NavLink> </li>
+                    <li><NavLink to='/favorites' activeStyle={{ display: 'none' }}>Favorites</NavLink> </li>
 
-                    <li><NavLink  to='/signup' onClick = {this.props.logout}>Log Out </NavLink> </li>
+                    <li><NavLink to='/aboutus' activeStyle={{ display: 'none' }}> About Us </NavLink> </li>
+
+                    <li><NavLink to='/login' onClick={this.props.logout}>Log Out </NavLink> </li>
 
 
-                </ul>          
-             
+                </ul>
+
             </div>
         )
     }
